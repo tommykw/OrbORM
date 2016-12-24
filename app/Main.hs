3e6e6a4f2d2b52009c7da24ce9a2ecd6522d2810
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-12-24 23:00:44 +0900

    fix

--- app/Main.hs
+++ app/Main.hs
@@ -1,65 +1,47 @@
-{-# LANGUAGE OverloadedStrings, FlexibleContexts, DeriveDataTypeable, QuasiQuotes #-}
+{-# LANGUAGE RankNTypes #-}
+
 module Main where
 
-import Prelude
-import Control.Lens.Action
-import Control.Monad (when, void)
-import Control.Monad.Trans.Reader (runReaderT)
-import Data.Configurator.Types (Config)
-import Data.Data (Data)
-import Data.Maybe (listToMaybe, isNothing, fromJust)
-import Data.Version (showVersion)
-import System.Console.CmdArgs.Explicit (HelpFormat(..), helpText)
-import Web.Twitter.Conduit (stream, statusesFilterByTrack)
+import Lib.Server (server)
+import Lib.Types (Port(..), ServerOptions(..))
 
-import qualified Data.Conduit as C
-import qualified Data.Conduit.List as CL
-import qualified Data.Configurator as Conf
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import qualified Network.HTTP.Conduit as HTTP
-import qualified System.Console.CmdArgs.Implicit as CA
+import Data.Default (def)
+import Data.Version (Version(), showVersion)
+import Options.Applicative
+import Paths_rss_markdown_proxy (version)
 
-data Ibox = Ibox { config :: FilePath }
-    deriving (Show, Data, Typeable)
+readPort :: forall a. ReadM (Port a)
+readPort = eitherReader $ \arg -> case reads arg of
+  [(r, "")] -> return $ Port r
+  _         -> Left $ "cannot parse port value `" <> arg <> "'"
 
-programName :: String
-programName = "ibox"
+serverParser :: Version -> ParserInfo ServerOptions
+serverParser ver =
+  info ( helper <*> appOpts <**> versionInfo )
+    ( fullDesc
+   <> progDesc "Reverse proxy for rendering Markdown in RSS feeds"
+   <> header "rss-markdown-proxy-server" )
+  where
+    appOpts = ServerOptions
+      <$> argument str (metavar "URL")
+      <*> option readPort
+          ( long "port"
+         <> short 'p'
+         <> value def
+         <> showDefault
+         <> help "HTTP proxy server port" )
+      <*> option readPort
+          ( long "metrics-port"
+         <> short 'm'
+         <> value def
+         <> showDefault
+         <> help "HTTP metrics server port" )
 
-args :: CA.Mode (CA.CmgArgs Ibox)
-args = CA.cmdArgsMode $ Ibox { config = CA.def CA.& = CA.args }
-    CA.& = CA.summary (unwords [programName, showVersion, version])
-    CA.& = CA.program programName
+    versionInfo = infoOption ( "rss-markdown-proxy-server " ++ showVersion ver )
+      ( short 'V'
+     <> long "version"
+     <> hidden
+     <> help "Show version information" )
 
 main :: IO ()
-main = do
-    mainArgs <- CA.cmdArgsRun args
-    let confFile = config mainArgs
-    if null confFile
-        then print $ helpText [] HelpFormatDefault args
-        else runBot confFile
-
-runBot :: FilePath -> IO()
-runBot confFile = do
-    conf <- Conf.load [Conf.Required confFile]
-    username <- Conf.lookupDefault "" conf "userName"
-    twInfo <- getTWInfoFromEnv conf
-    userId <- getUserId conf
-    when (isNothing userId) $ error "accessToken must contain a'-'"
-    void . HTTP.withManager $ \mngr -> do
-        leftIO . T.putStrLn $ T.unwords ["Listening for Tweets to", username, "..."]
-
-    let lenv = LtxbotEnv {
-        envUserId = fromJust userId,
-        envTwInfo = twInfo,
-        envManager = mngr
-    }
-
-    src <- stream twInfo mngr (statusesFilterByTrack $ T.contact ["@", username])
-    src C.$ =+ normalizeMentions C.$$+- CL.mapM_ (^! act ((~runReaderT`) lenv) . actTL)
-
-where
-    getUserId :: Config -> IO (Maybe UserId)
-    getUserId conf = do
-        maybeUid <- liftIO $ fmap (listToMaybe . T.split (== '-')) (Conf.lookupDefalut "" conf "accessToken")
-        return $ fmap (read . T.unpack) maybeUid
+main = execParser (serverParser version) >>= server
